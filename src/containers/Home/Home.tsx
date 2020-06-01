8c88b190a2a37006eb851cef1339e51ba9d22ecb
Jose Avilez <jose.avilez@partnerhero.com>
2020-06-01 03:00:10 -0600

    Add material table and logic
    Added material table to the project and some logic for the dashboard.

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,26 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import Moment from 'moment';
-
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import MaterialTable, { Column } from "material-table";
+import Moment from "moment";
 
 import { fbAuth } from "../../services/firebase";
 
 import MoviesData from "../../data/movies.json";
-import { TablePagination } from "@material-ui/core";
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
 
 interface TableMovieData {
   title: string;
@@ -31,6 +16,14 @@ interface TableMovieData {
   genres: Array<string>;
 }
 
+const movieGenres: Array<string> = [];
+
+const addMovieGenre = (genres: Array<string>): void => {
+  genres.forEach((genre) => {
+    if (!movieGenres.includes(genre)) movieGenres.push(genre);
+  });
+};
+
 const createData = (
   title: string,
   year: string,
@@ -39,6 +32,7 @@ const createData = (
   rating: string,
   genres: Array<string>
 ): TableMovieData => {
+  addMovieGenre(genres);
   return {
     title,
     year: parseInt(year, 10),
@@ -60,70 +54,69 @@ const tableRows = MoviesData.map((movie) =>
   )
 );
 
+const tableColumns: Column<TableMovieData>[] = [
+  {
+    title: "Title",
+    field: "title",
+  },
+  {
+    title: "Year",
+    field: "year",
+    filtering: false,
+  },
+  {
+    title: "Runtime",
+    field: "runtime",
+    render: (rowData) =>
+      Moment.utc()
+        .startOf("day")
+        .add({ minutes: rowData.runtime })
+        .format("H[h] mm[m]"),
+    filtering: false,
+  },
+  {
+    title: "Revenue",
+    field: "revenue",
+    render: (rowData) => `$${rowData.revenue} M`,
+    filtering: false,
+  },
+  {
+    title: "Rating",
+    field: "rating",
+    filtering: false,
+  },
+  {
+    title: "Genres",
+    field: "genres",
+    render: (rowData) =>
+      rowData.genres.map((genre, index) =>
+        rowData.genres.length - 1 !== index ? `${genre}, ` : genre
+      ),
+    customFilterAndSearch: (term, rowData) => rowData.genres.includes(term),
+  },
+];
+
 const Home: React.FC = () => {
   const history = useHistory();
-  const classes = useStyles();
-
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(25);
 
   const handleLogout = (): void => {
     fbAuth.signOut();
     history.push("/");
   };
 
-  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
   return (
     <div>
       <p>This is home</p>
       <p>{tableRows.length} movies in the database</p>
+      <p>{movieGenres}</p>
       <button onClick={handleLogout}>Logout</button>
 
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Title</TableCell>
-              <TableCell align="right">Year</TableCell>
-              <TableCell align="right">Runtime</TableCell>
-              <TableCell align="right">Revenue</TableCell>
-              <TableCell align="right">Rating</TableCell>
-              <TableCell align="right">Genres</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {tableRows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-              <TableRow key={`${row.revenue}${row.title}`}>
-                <TableCell component="th" scope="row">
-                  {row.title}
-                </TableCell>
-                <TableCell align="right">{row.year}</TableCell>
-                <TableCell align="right">{Moment.utc().startOf('day').add({ minutes: row.runtime }).format('H[h] mm[m]')}</TableCell>
-                <TableCell align="right">${row.revenue} M</TableCell>
-                <TableCell align="right">{row.rating}</TableCell>
-                <TableCell align="right">{row.genres}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={tableRows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onChangePage={handleChangePage}
-          onChangeRowsPerPage={handleChangeRowsPerPage}
-        />
+      <MaterialTable
+        title="Movies Information"
+        columns={tableColumns}
+        data={tableRows}
+        options={{ filtering: true, search: false }}
+      />
     </div>
   );
 };
