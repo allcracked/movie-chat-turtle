5398de968fdb464a83af194dc889b78cf3d1e4c1
Jose Avilez <jose.avilez@partnerhero.com>
2020-06-01 01:12:50 -0600

    Add movies data and interfaces
    Added the movies data and interfaces into the project.

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
-import { fbAuth } from '../../services/firebase';
+import { fbAuth } from "../../services/firebase";
+import Movie from "../../data/interfaces";
+
+import MoviesData from "../../data/movies.json";
 
 const Home: React.FC = () => {
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleLogout = (): void => {
-        fbAuth.signOut();
-        history.push('/');
-    };
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-    return (
-        <div>
-            <p>This is home</p>
-            <button onClick={handleLogout}>Logout</button>
-        </div>
-    );
+  const handleLogout = (): void => {
+    fbAuth.signOut();
+    history.push("/");
+  };
+
+  useEffect(() => {
+    const obtainedMovies = MoviesData as Movie[];
+    setMovies(obtainedMovies);
+  }, []);
+
+  return (
+    <div>
+      <p>This is home</p>
+      <p>{movies.length} movies in the database</p>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 };
 
 export default Home;
